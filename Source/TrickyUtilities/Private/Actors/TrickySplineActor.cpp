61d6bd528e4542e8674e9d5c732eda573ff1a9c7
Artyom Volkov <tricky.fat.cat@gmail.com>
2025-04-17 20:17:23 +0300

    Create spline actor

---
+++ Source/TrickyUtilities/Private/Actors/TrickySplineActor.cpp
@@ -0,0 +1,14 @@
+﻿// MIT License Copyright (c) Artyom "Tricky Fat Cat" Volkov
+
+
+#include "Actors/TrickySplineActor.h"
+
+#include "Components/SplineComponent.h"
+
+
+ATrickySplineActor::ATrickySplineActor()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	SplineComponent = CreateDefaultSubobject<USplineComponent>(TEXT("SplineComponent"));
+}
---
+++ Source/TrickyUtilities/Public/Actors/TrickySplineActor.h
@@ -0,0 +1,22 @@
+﻿// MIT License Copyright (c) Artyom "Tricky Fat Cat" Volkov
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "TrickySplineActor.generated.h"
+
+class USplineComponent;
+
+UCLASS()
+class TRICKYUTILITIES_API ATrickySplineActor : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	ATrickySplineActor();
+
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	TObjectPtr<USplineComponent> SplineComponent = nullptr;
+};
