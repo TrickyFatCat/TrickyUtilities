a7af8dec373bb6f92f0fbb47d2ce648eda992a48
Artyom Volkov <tricky.fat.cat@gmail.com>
2025-04-19 21:32:47 +0300

    Create an annotation actor

---
+++ Source/TrickyUtilities/Private/Actors/TrickyAnnotationActor.cpp
@@ -0,0 +1,255 @@
+﻿// MIT License Copyright (c) Artyom "Tricky Fat Cat" Volkov
+
+
+#include "Actors/TrickyAnnotationActor.h"
+
+#include "Components/TextRenderComponent.h"
+
+
+ATrickyAnnotationActor::ATrickyAnnotationActor()
+{
+	PrimaryActorTick.bCanEverTick = false;
+	SetCanBeDamaged(false);
+
+	RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
+
+	FrontMeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("FrontMesh"));
+	FrontMeshComponent->SetupAttachment(GetRootComponent());
+	SetupComponent(FrontMeshComponent);
+
+	BackMeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("BackMesh"));
+	BackMeshComponent->SetupAttachment(GetRootComponent());
+	SetupComponent(BackMeshComponent);
+
+	if (FrontMeshComponent && BackMeshComponent)
+	{
+		struct FMeshConstructorStatics
+		{
+			ConstructorHelpers::FObjectFinder<UStaticMesh> StaticMesh;
+			FName ID_Misc;
+			FText NAME_Misc;
+
+			FMeshConstructorStatics()
+				: StaticMesh(TEXT("/Engine/BasicShapes/Plane"))
+				  , ID_Misc(TEXT("Misc"))
+				  , NAME_Misc(NSLOCTEXT("StaticMeshCategory", "Misc", "Misc"))
+			{
+			}
+		};
+
+		static FMeshConstructorStatics ConstructorStatics;
+		UMaterialInterface* Material = LoadObject<UMaterialInterface>(
+			this, TEXT("/Engine/EngineDebugMaterials/BlackUnlitMaterial"));
+
+		FrontMeshComponent->SetStaticMesh(ConstructorStatics.StaticMesh.Object);
+		FrontMeshComponent->SetMaterial(0, Material);
+		FrontMeshComponent->SetRelativeLocation(FVector(1, 0, 0));
+		FrontMeshComponent->SetRelativeRotation(FRotator(-90, 0, 0));
+
+		BackMeshComponent->SetStaticMesh(ConstructorStatics.StaticMesh.Object);
+		BackMeshComponent->SetMaterial(0, Material);
+		BackMeshComponent->SetRelativeLocation(FVector(-1, 0, 0));
+		BackMeshComponent->AddRelativeRotation(FRotator(90, 0, 0));
+	}
+
+	FrontTextComponent = CreateDefaultSubobject<UTextRenderComponent>(TEXT("FrontText"));
+	FrontTextComponent->SetupAttachment(GetRootComponent());
+	SetupComponent(FrontTextComponent);
+
+	BackTextComponent = CreateDefaultSubobject<UTextRenderComponent>(TEXT("BackText"));
+	BackTextComponent->SetupAttachment(GetRootComponent());
+	SetupComponent(BackTextComponent);
+
+	if (FrontTextComponent && BackTextComponent)
+	{
+		TextMaterial = LoadObject<UMaterialInterface>(
+			this, TEXT("/Engine/EditorResources/FieldNodes/_Resources/DefaultTextMaterialOpaque"));
+
+
+		FrontTextComponent->SetRelativeLocation(FVector(2, 0, 0));
+		UpdateTextProperties(FrontTextComponent,
+		                     AnnotationText,
+		                     TextSize,
+		                     TextColor,
+		                     VerticalAlignment,
+		                     HorizontalAlignment,
+		                     TextMaterial);
+		UpdateTextHorizontalPosition(FrontTextComponent, false);
+		UpdateTextVerticalPosition(FrontTextComponent);
+
+		BackTextComponent->SetRelativeLocation(FVector(-2, 0, 0));
+		BackTextComponent->SetRelativeRotation(FRotator(0, 180, 0));
+		UpdateTextProperties(BackTextComponent,
+		                     AnnotationText,
+		                     TextSize,
+		                     TextColor,
+		                     VerticalAlignment,
+		                     HorizontalAlignment,
+		                     TextMaterial);
+		UpdateTextHorizontalPosition(BackTextComponent, true);
+		UpdateTextVerticalPosition(BackTextComponent);
+
+		UpdateBackgroundSize(FrontMeshComponent);
+		UpdateBackgroundSize(BackMeshComponent);
+	}
+}
+
+void ATrickyAnnotationActor::OnConstruction(const FTransform& Transform)
+{
+	Super::OnConstruction(Transform);
+
+	UpdateTextProperties(FrontTextComponent,
+	                     AnnotationText,
+	                     TextSize,
+	                     TextColor,
+	                     VerticalAlignment,
+	                     HorizontalAlignment,
+	                     TextMaterial);
+	UpdateTextHorizontalPosition(FrontTextComponent, false);
+	UpdateTextVerticalPosition(FrontTextComponent);
+
+	UpdateTextProperties(BackTextComponent,
+	                     AnnotationText,
+	                     TextSize,
+	                     TextColor,
+	                     VerticalAlignment,
+	                     HorizontalAlignment,
+	                     TextMaterial);
+	UpdateTextHorizontalPosition(BackTextComponent, true);
+	UpdateTextVerticalPosition(BackTextComponent);
+
+	UpdateBackgroundSize(FrontMeshComponent);
+	UpdateBackgroundSize(BackMeshComponent);
+}
+
+void ATrickyAnnotationActor::SetAnnotationText(const FText& NewText)
+{
+	AnnotationText = NewText;
+	FrontTextComponent->SetText(AnnotationText);
+	BackTextComponent->SetText(AnnotationText);
+}
+
+void ATrickyAnnotationActor::SetTextColor(const FColor& NewColor)
+{
+	TextColor = NewColor;
+	FrontTextComponent->SetTextRenderColor(TextColor);
+	BackTextComponent->SetTextRenderColor(TextColor);
+}
+
+void ATrickyAnnotationActor::UpdateTextProperties(UTextRenderComponent* Component,
+                                                  const FText& Text,
+                                                  const int32 Size,
+                                                  const FColor& Color,
+                                                  const EVerticalTextAligment& VertTextAlignment,
+                                                  const EHorizTextAligment& HorizTextAlignment,
+                                                  UMaterialInterface* Material)
+{
+	if (!IsValid(Component))
+	{
+		return;
+	}
+
+	Component->SetText(AnnotationText);
+	Component->SetTextMaterial(TextMaterial);
+	Component->SetWorldSize(TextSize);
+	Component->SetTextRenderColor(TextColor);
+	Component->SetVerticalAlignment(VerticalAlignment);
+	Component->SetHorizontalAlignment(HorizontalAlignment);
+
+	if (IsValid(Material))
+	{
+		Component->SetTextMaterial(Material);
+	}
+}
+
+void ATrickyAnnotationActor::UpdateBackgroundSize(UStaticMeshComponent* Component) const
+{
+	if (!IsValid(Component))
+	{
+		return;
+	}
+
+	const FVector TextLocalSize = FrontTextComponent->GetTextLocalSize();
+	FVector NewScale = FVector::OneVector;
+	NewScale.X = (TextLocalSize.Z / 100.f) + TextOffset;
+	NewScale.Y = (TextLocalSize.Y / 100.f) + TextOffset;
+	Component->SetRelativeScale3D(NewScale);
+}
+
+void ATrickyAnnotationActor::UpdateTextHorizontalPosition(UTextRenderComponent* Component, const bool bNegate) const
+{
+	if (!IsValid(Component))
+	{
+		return;
+	}
+
+	float CurrentDisplacementFactor = 0.f;
+
+	switch (HorizontalAlignment)
+	{
+	case EHTA_Left:
+		CurrentDisplacementFactor = DisplacementFactor;
+		break;
+
+	case EHTA_Right:
+		CurrentDisplacementFactor = -DisplacementFactor;
+		break;
+
+	default:
+		CurrentDisplacementFactor = 0.f;
+		break;
+	}
+
+	FVector NewLocation = Component->GetRelativeLocation();
+	NewLocation.Y = CurrentDisplacementFactor * Component->GetTextLocalSize().Y;
+
+	if (bNegate)
+	{
+		NewLocation.Y *= -1;
+	}
+
+	Component->SetRelativeLocation(NewLocation);
+}
+
+void ATrickyAnnotationActor::UpdateTextVerticalPosition(UTextRenderComponent* Component) const
+{
+	if (!IsValid(Component))
+	{
+		return;
+	}
+
+	float CurrentDisplacementFactor = 0.f;
+
+	switch (VerticalAlignment)
+	{
+	case EVRTA_TextTop:
+		CurrentDisplacementFactor = DisplacementFactor;
+		break;
+
+	case EVRTA_TextBottom:
+		CurrentDisplacementFactor = -DisplacementFactor;
+		break;
+
+	default:
+		CurrentDisplacementFactor = 0.f;
+		break;
+	}
+
+	FVector NewLocation = Component->GetRelativeLocation();
+	NewLocation.Z = CurrentDisplacementFactor * Component->GetTextLocalSize().Z;
+	Component->SetRelativeLocation(NewLocation);
+}
+
+void ATrickyAnnotationActor::SetupComponent(UPrimitiveComponent* Component) const
+{
+	if (!IsValid(Component))
+	{
+		return;
+	}
+	
+	Component->SetupAttachment(GetRootComponent());
+	Component->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	Component->SetCollisionResponseToAllChannels(ECR_Ignore);
+	Component->SetCastShadow(false);
+	Component->SetReceivesDecals(false);
+}
---
+++ Source/TrickyUtilities/Public/Actors/TrickyAnnotationActor.h
@@ -0,0 +1,108 @@
+﻿// MIT License Copyright (c) Artyom "Tricky Fat Cat" Volkov
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/TextRenderComponent.h"
+#include "GameFramework/Actor.h"
+#include "TrickyAnnotationActor.generated.h"
+
+enum EHorizTextAligment : int;
+enum EVerticalTextAligment : int;
+class UTextRenderComponent;
+
+UCLASS(HideCategories=(Actor, Collision, Input, Replication, HLOD, WorldPartition, DataLayers, Physics,
+	Networking, LevelInstance))
+class TRICKYUTILITIES_API ATrickyAnnotationActor : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	ATrickyAnnotationActor();
+
+protected:
+	virtual void OnConstruction(const FTransform& Transform) override;
+
+public:
+	UFUNCTION(BlueprintGetter, Category = "TrickyAnnotation")
+	FText GetAnnotationText() const { return AnnotationText; };
+
+	UFUNCTION(BlueprintSetter, Category = "TrickyAnnotation")
+	void SetAnnotationText(const FText& NewText);
+
+	UFUNCTION(BlueprintGetter, Category = "TrickyAnnotation")
+	FColor GetTextColor() const { return TextColor; };
+
+	UFUNCTION(BlueprintSetter, Category = "TrickyAnnotation")
+	void SetTextColor(const FColor& NewColor);
+
+protected:
+	UPROPERTY()
+	TObjectPtr<UStaticMeshComponent> FrontMeshComponent = nullptr;
+
+	UPROPERTY()
+	TObjectPtr<UStaticMeshComponent> BackMeshComponent = nullptr;
+
+	UPROPERTY()
+	TObjectPtr<UTextRenderComponent> FrontTextComponent = nullptr;
+
+	UPROPERTY()
+	TObjectPtr<UTextRenderComponent> BackTextComponent = nullptr;
+
+	UPROPERTY(EditAnywhere,
+		BlueprintGetter=GetAnnotationText,
+		BlueprintSetter=SetAnnotationText,
+		Category = "TrickyAnnotation",
+		meta=(MultiLine))
+	FText AnnotationText = FText::FromString("Annotation Text");
+
+	UPROPERTY(EditAnywhere,
+		BlueprintReadOnly,
+		Category = "TrickyAnnotation",
+		meta=(ClampMin=1, UIMin=1, ClampMax=128, UIMax=128))
+	int32 TextSize = 32;
+
+	UPROPERTY(EditAnywhere,
+		BlueprintGetter=GetTextColor,
+		BlueprintSetter=SetTextColor,
+		Category = "TrickyAnnotation",
+		meta=(HideAlphaChannel))
+	FColor TextColor = FColor::White;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "TrickyAnnotation")
+	TEnumAsByte<EVerticalTextAligment> VerticalAlignment = EVerticalTextAligment::EVRTA_TextCenter;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "TrickyAnnotation")
+	TEnumAsByte<EHorizTextAligment> HorizontalAlignment = EHorizTextAligment::EHTA_Center;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "TrickyAnnotation", AdvancedDisplay)
+	UMaterialInterface* TextMaterial = nullptr;
+
+private:
+	const float DisplacementFactor = 0.5f;
+
+	const float TextOffset = 0.25f;
+	
+	UFUNCTION()
+	void UpdateTextProperties(UTextRenderComponent* Component,
+	                          const FText& Text,
+	                          const int32 Size,
+	                          const FColor& Color,
+	                          const EVerticalTextAligment& VertTextAlignment,
+	                          const EHorizTextAligment& HorizTextAlignment,
+	                          UMaterialInterface* Material);
+
+	UFUNCTION()
+	void UpdateBackgroundSize(UStaticMeshComponent* Component) const;
+
+	UFUNCTION()
+	void UpdateTextHorizontalPosition(UTextRenderComponent* Component, const bool bNegate) const;
+
+	UFUNCTION()
+	void UpdateTextVerticalPosition(UTextRenderComponent* Component) const;
+
+	UFUNCTION()
+	void SetupComponent(UPrimitiveComponent* Component) const;
+
+
+};
