2a30d44762a97a18b90c859be85388eadb239d79
Artyom Volkov <tricky.fat.cat@gmail.com>
2025-04-20 13:27:31 +0300

    Add time formatting function

--- Source/TrickyUtilities/Private/TrickyUtilityLibrary.cpp
+++ Source/TrickyUtilities/Private/TrickyUtilityLibrary.cpp
@@ -4,6 +4,7 @@
 #include "TrickyUtilityLibrary.h"
 
 #include "Components/TimelineComponent.h"
+#include "Kismet/KismetMathLibrary.h"
 
 void UTrickyUtilityLibrary::CalculateTimelinePlayRate(UTimelineComponent* Timeline, const float DesiredTime)
 {
@@ -21,4 +22,53 @@ void UTrickyUtilityLibrary::CalculateTimelinePlayRate(UTimelineComponent* Timeli
 
 	const float PlayRate = TimelineLength / DesiredTime;
 	Timeline->SetPlayRate(PlayRate);
+}
+
+void UTrickyUtilityLibrary::FormatTimeSeconds(const float TimeSeconds,
+                                               const ETimeFormat TimeFormat,
+                                               FString& OutResult)
+{
+	const FTimespan Timespan = UKismetMathLibrary::FromSeconds(TimeSeconds);
+	const int32 TotalMinutes = static_cast<int32>(Timespan.GetTotalMinutes());
+	const int32 Seconds = Timespan.GetSeconds();
+	const int32 TotalSeconds = (FMath::CeilToInt(Timespan.GetTotalSeconds()));
+	const int32 Milliseconds = Timespan.GetFractionMilli();
+
+	auto ConvertMilliseconds = [&Milliseconds](const float Fraction) -> int32
+	{
+		return static_cast<int32>(Milliseconds * Fraction);
+	};
+
+	switch (TimeFormat)
+	{
+	case ETimeFormat::MM_SS_MsMs:
+		OutResult = FString::Printf(TEXT("%02d:%02d.%02d"),
+		                         TotalMinutes,
+		                         Seconds,
+		                         ConvertMilliseconds(0.1f));
+		break;
+
+	case ETimeFormat::MM_SS_Ms:
+		OutResult = FString::Printf(TEXT("%02d:%02d.%d"),
+		                         TotalMinutes,
+		                         Seconds,
+		                         ConvertMilliseconds(0.01f));
+		break;
+
+	case ETimeFormat::MM_SS:
+		OutResult = FString::Printf(TEXT("%02d:%02d"), TotalMinutes, Seconds);
+		break;
+
+	case ETimeFormat::SS_MsMs:
+		OutResult = FString::Printf(TEXT("%02d.%02d"), TotalSeconds, ConvertMilliseconds(0.1f));
+		break;
+
+	case ETimeFormat::SS_Ms:
+		OutResult = FString::Printf(TEXT("%02d.%d"), TotalSeconds, ConvertMilliseconds(0.01f));
+		break;
+
+	case ETimeFormat::SS:
+		OutResult = FString::Printf(TEXT("%02d"), TotalSeconds);
+		break;
+	}
 }
--- Source/TrickyUtilities/Public/TrickyUtilityLibrary.h
+++ Source/TrickyUtilities/Public/TrickyUtilityLibrary.h
@@ -7,6 +7,18 @@
 #include "TrickyUtilityLibrary.generated.h"
 
 class UTimelineComponent;
+
+UENUM(BlueprintType)
+enum class ETimeFormat : uint8
+{
+	MM_SS_MsMs UMETA(DisplayName="MM:SS.MsMs"),
+	MM_SS_Ms UMETA(DisplayName="MM:SS.Ms"),
+	MM_SS UMETA(DisplayName="MM:SS"),
+	SS_MsMs UMETA(DisplayName="SS.MsMs"),
+	SS_Ms UMETA(DisplayName="SS.Ms"),
+	SS UMETA(DisplayName="SS")
+};
+
 /**
  * 
  */
@@ -23,11 +35,19 @@ public:
 	 * then assigns the calculated rate to the timeline. If the input timeline is invalid,
 	 * or if the desired time or timeline length is zero or negative, the function does nothing.
 	 *
-	 * @param Timeline A pointer to the UTimelineComponent whose play rate will be adjusted.
-	 *                 Must be a valid instance.
-	 * @param DesiredTime The desired duration in seconds for the timeline playback.
-	 *                    Must be greater than 0.
+	 * @param Timeline A pointer to the UTimelineComponent whose play rate will be adjusted. Must be a valid instance.
+	 * @param DesiredTime The desired duration in seconds for the timeline playback. Must be greater than 0.
 	 */
 	UFUNCTION(BlueprintCallable, Category="TrickyUtilities")
 	static void CalculateTimelinePlayRate(UTimelineComponent* Timeline, const float DesiredTime);
+
+	/**
+	 * Formats a given time duration in seconds into a string in the specified format.
+	 *
+	 * @param TimeSeconds The time in seconds that needs to be formatted. Must be greater than 0.
+	 * @param TimeFormat The desired format.
+	 * @param OutResult A reference to a string where the formatted time will be stored.
+	 */
+	UFUNCTION(BlueprintPure, Category="TrickyUtilities")
+	static void FormatTimeSeconds(const float TimeSeconds, const ETimeFormat TimeFormat, FString& OutResult);	
 };
